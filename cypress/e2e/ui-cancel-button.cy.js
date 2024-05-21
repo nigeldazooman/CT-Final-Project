f3205db9039a30aabde49e64af97b979e170b931
Brad N. <150868921+nigeldazooman@users.noreply.github.com>
2024-05-20 20:02:37 -0400

    updating cancel button test

--- cypress/e2e/ui-cancel-button.cy.js
+++ cypress/e2e/ui-cancel-button.cy.js
@@ -1,11 +1,31 @@
 describe("Functional UI Testing with Cypress", () => {
-    it("Tests the Cancel Button", () => {
-      cy.visit('https://thinking-tester-contact-list.herokuapp.com/');
-      cy.get('#email').type('brad@brightskyefarm.com');
-      cy.get('#password').type('Lindy11');
-      cy.get('#submit').click();
-      cy.get(':nth-child(3) > :nth-child(2)').click();
-      cy.get('#edit-contact').click();
-      cy.get('#cancel').click();
-      cy.get('h1').should('have.text','Contact Details');
-})});
+  it("Tests the Cancel Button", () => {
+    cy.visit('https://thinking-tester-contact-list.herokuapp.com/');
+    // Login
+    cy.get('#email').type('brad@brightskyefarm.com');
+    cy.get('#password').type('Lindy11');
+    cy.get('#submit').click();
+    // Wait for the contact list to be fetched and rendered
+    cy.intercept('GET', '**/contacts').as('getContacts');
+    cy.wait('@getContacts');
+    // Wait for the element to be visible and interact
+    cy.get(':nth-child(3) > :nth-child(2)', { timeout: 10000 }).should('be.visible').then(($el) => {
+      cy.wrap($el).click();
+    });
+    // Edit contact
+    cy.get('#edit-contact', { timeout: 10000 }).should('be.visible').then(($el) => {
+      cy.wrap($el).click();
+    });
+    // Cancel edit
+    cy.get('#cancel', { timeout: 10000 }).should('be.visible').then(($el) => {
+      cy.wrap($el).click();
+    });
+    // Verify the text
+    cy.get('h1', { timeout: 10000 }).should('have.text', 'Contact Details');
+  });
+});
+
+
+
+
+
